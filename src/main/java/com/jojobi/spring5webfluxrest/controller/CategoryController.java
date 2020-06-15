952fd1933ea0b5a174b7f9a6d77aeb66a0e667e8
lakemaster <lakelord@jojobi.de>
2020-06-15 10:10:41 +0200

    add Catgorie Post Method and Test

--- src/main/java/com/jojobi/spring5webfluxrest/controller/CategoryController.java
+++ src/main/java/com/jojobi/spring5webfluxrest/controller/CategoryController.java
@@ -2,9 +2,9 @@ package com.jojobi.spring5webfluxrest.controller;
 
 import com.jojobi.spring5webfluxrest.domain.Category;
 import com.jojobi.spring5webfluxrest.repositories.CategoryRepository;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RestController;
+import org.reactivestreams.Publisher;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.*;
 import reactor.core.publisher.Flux;
 import reactor.core.publisher.Mono;
 
@@ -27,4 +27,11 @@ public class CategoryController {
     public Mono<Category> findById(@PathVariable("id") String id) {
         return categoryRepository.findById(id);
     }
+
+    @ResponseStatus(HttpStatus.CREATED)
+    @PostMapping("/api/v1/categories")
+    public Mono<Void> create(@RequestBody Publisher<Category> categoryStream) {
+        return categoryRepository.saveAll(categoryStream).then();
+    }
+
 }
--- src/test/java/com/jojobi/spring5webfluxrest/controller/CategoryControllerTest.java
+++ src/test/java/com/jojobi/spring5webfluxrest/controller/CategoryControllerTest.java
@@ -7,6 +7,7 @@ import org.junit.jupiter.api.Test;
 import org.mockito.ArgumentMatchers;
 import org.mockito.BDDMockito;
 import org.mockito.Mockito;
+import org.reactivestreams.Publisher;
 import org.springframework.test.web.reactive.server.WebTestClient;
 import reactor.core.publisher.Flux;
 import reactor.core.publisher.Mono;
@@ -14,6 +15,7 @@ import reactor.core.publisher.Mono;
 import java.util.function.Consumer;
 
 import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.ArgumentMatchers.anyString;
 
 class CategoryControllerTest {
@@ -55,5 +57,19 @@ class CategoryControllerTest {
                 .exchange()
                 .expectBody(Category.class)
                 .isEqualTo(category);
+    }
+
+    @Test
+    void testCreate() {
+        BDDMockito.given(categoryRepository.saveAll(any(Publisher.class)))
+                .willReturn(Flux.just(Category.builder().build()));
+
+        Mono<Category> categoryMono = Mono.just(Category.builder().build());
+
+        webTestClient.post()
+                .uri("/api/v1/categories")
+                .body(categoryMono, Category.class)
+                .exchange()
+                .expectStatus().isCreated();
     }
 }
